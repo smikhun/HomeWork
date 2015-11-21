2952a356b501ad5db8dd63affe049d821cfb0a7e
smikhun <a.smikhun@gmail.com>
2015-11-21 13:16:12 +0200

    Create book.java

---
+++ book.java
@@ -0,0 +1,21 @@
+package ua.artcode.library;
+
+/**
+ * Created by Andriy on 19.11.2015.
+ */
+public class Book {
+    String name;
+    String autor;
+    int year;
+    Boolean status;
+
+    Book(String name, String autor, int year, Boolean status){
+            
+            this.name = name;
+            this.autor = autor;
+            this.year = year;
+            this.status = status;
+    }
+
+
+}
